eff76dc80e9e3c2069c43f1f0a1fcef694221fc8
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-05-24 14:05:07 -0300

    first commit

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tab_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+}
