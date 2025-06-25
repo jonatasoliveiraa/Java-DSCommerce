2d7f723ad005110e0c185f50a3c8f36625eefe92
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-06-25 20:00:12 -0300

    Create and implements OrderDTO and get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public OrderItemDTO(OrderItem entity) {
+        this.id = entity.getProduct().getId();
+        this.name = entity.getProduct().getName();
+        this.price = entity.getPrice();
+        this.quantity = entity.getQuantity();
+    }
+
+    public Double getSubtotal() {
+        return price * quantity;
+    }
+}
